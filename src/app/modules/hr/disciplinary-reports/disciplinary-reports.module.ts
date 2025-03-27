b55a086246834e3c41b93143fd242b0c3e1175c3
Mariana-del-Rocio-Copca-Rojas <copcarojasmariana@gmail.com>
2025-03-27 13:17:14 -0600

    reportes disciplinarios

--- src/app/modules/hr/disciplinary-reports/disciplinary-reports.module.ts
+++ src/app/modules/hr/disciplinary-reports/disciplinary-reports.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router'; // <-- Agrega esta línea
+
 import { DisciplinaryReportsComponent } from './pages/disciplinary-reports/disciplinary-reports.component';
+import { NewDisciplinaryReportComponent } from './pages/new-disciplinary-report/new-disciplinary-report.component';
 
 @NgModule({
-  declarations: [DisciplinaryReportsComponent],
-  imports: [CommonModule],
+  declarations: [
+    DisciplinaryReportsComponent,
+    NewDisciplinaryReportComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule  // <-- y esta línea
+  ],
   exports: [DisciplinaryReportsComponent]
 })
 export class DisciplinaryReportsModule { }
