d1137fa13ed3445798378987ea14800b9bf105b8
Mariana <L20350260@veracruz.tecnm.mx>
2025-03-30 22:15:16 -0600

    error resuelto

--- src/app/modules/hr/disciplinary-reports/disciplinary-reports.module.ts
+++ src/app/modules/hr/disciplinary-reports/disciplinary-reports.module.ts
@@ -4,17 +4,15 @@ import { RouterModule } from '@angular/router'; // <-- Agrega esta línea
 
 import { DisciplinaryReportsComponent } from './pages/disciplinary-reports/disciplinary-reports.component';
 import { NewDisciplinaryReportComponent } from './pages/new-disciplinary-report/new-disciplinary-report.component';
-import { EmployeeSearchModalComponent } from './components/employee-search-modal/employee-search-modal.component';
 
 @NgModule({
   declarations: [
     DisciplinaryReportsComponent,
     NewDisciplinaryReportComponent,
-    EmployeeSearchModalComponent
   ],
   imports: [
     CommonModule,
-    RouterModule  // <-- y esta línea
+    RouterModule 
   ],
   exports: [DisciplinaryReportsComponent]
 })
