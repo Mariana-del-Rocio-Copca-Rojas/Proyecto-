3bd1dfaec65e283d9c5319dc635381778ef92603
ResidenteIvan <mesai132002@gmail.com>
2025-03-31 09:26:37 -0600

    Subiendo proyecto Angular inicial

--- src/app/modules/hr/recruitment/pages/requisition/requisition.component.ts
+++ src/app/modules/hr/recruitment/pages/requisition/requisition.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-requisition',
+  standalone: false,
   templateUrl: './requisition.component.html',
-  styleUrls: ['./requisition.component.scss']
+  styleUrl: './requisition.component.scss'
 })
 export class RequisitionComponent {
-  // Aquí irá la lógica del componente de Requisición de Personal
+
 }
